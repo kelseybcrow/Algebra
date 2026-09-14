4f13cb5de2789100b9c586a41266224303942593
Kelsey Crow <kelseybcrow@gmail.com>
2026-09-14 16:21:40 -0600

    monday afternoon

---
+++ graphs-functions.py
@@ -0,0 +1,40 @@
+'''
+next problem
+'''
+    f(x)  =  4x² + (-1)
+'''
+function translation: [vertical] ➜
+    moving upward   ➜  add units to the tail end
+    moving downward   ➜  subtract units from the tail end
+'''
+'''
+function translation: [horizontal] ➜
+    moving left   ➜  add units inside
+    moving right  ➜  subtract units inside 
+
+''' 
+    h(x)  =  4x² + (-1) + 3
+    [h(x)  =  4x² + 2]
+          
+
+'''
+next problem
+'''
+    vertical upward 4-units
+
+    h(x)  =  (-2x²) + (-9)
+    f(x)  =  (-2x²) + (-9) + 4
+    [f(x)  =  (-2x²) + (-5)]
+
+
+'''
+next problem
+'''
+    vertical downward 4-units
+
+    f(x)  =  (-3x²) + 5 + (-4)
+    [h(x)  =  (-3x²) + 1]
+
+
+
+
