ce1110d63cd868e773c373aa639f78d8049f5312
Kelsey Crow <kelseybcrow@gmail.com>
2026-09-15 22:20:14 -0600

    tuesday evening

--- graphs-functions.py
+++ graphs-functions.py
@@ -32,9 +32,75 @@ next problem
 '''
     vertical downward 4-units
 
+    f(x)  =  (-3x²) + 5 
     f(x)  =  (-3x²) + 5 + (-4)
     [h(x)  =  (-3x²) + 1]
 
+'''
+next problem
+'''
+    (x = (-1))
+    (x = 1)
+    y = (-2)x²
+    [0, 0]
+
+#vertex is (0,0) because no numbers are added or subtracted after the x²
+
+    y = (-2)(-1)²
+    y = (-2)(-1)²
+    y = (-2)(1)
+    y = (-2)
+    [(-1), (-2)]
+
+    y = (-2)(1)²
+    y = (-2)(1)
+    y = (-2)
+    [(1), (-2)]
+
+
+    y = (-2)(2)²
+    y = (-2)(4)
+    y = (-8)
+    [(2), (-8)]
+    
+    y = (-2)(-2)²
+    y = (-2)(-8)
+    [(-2), (-8)]
+
+    '''
+    next problem
+    '''
+    y = x²
+
+#vertex is (0,0) because no numbers are added or subtracted after the x²
+'''
+right 1, up 1,3,5 shortcut
+'''
+    [0, 0]
+    [1, 1]
+    [2, 4]
+    [3, 9]
+'''
+mirror the points across the vertical center line
+'''
+    [-1, 1]
+    [-2, 4]
+    [-3, 9]
+    
+'''
+next problem
+'''
+'''
+right 1, up (1/3),(3/3),(5/3) shortcut
+'''
+    y = (1/3)x²
+    [0, 0]
+    [1, (1/3)]
+    [2, 4/3]
+    [3, 9/3]
+    [-1, (1/3)]
+    [-2, 4/3]
+    [-3, 9/3]
 
 
 
