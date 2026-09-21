f233caead096ab8014ba1caa29222e5d060930ae
Kelsey Crow <kelseybcrow@gmail.com>
2026-09-20 22:45:08 -0600

    sunday evening

---
+++ knowledge-checks/knowledge-check-sep-20.py
@@ -0,0 +1,103 @@
+    
+'''
+next
+'''
+    -(-2)³ =
+    -(-8) =
+    [8]
+
+    -(8)² =
+    -(64) =
+    [(-64)]
+    
+'''
+next
+'''
+    ((-7w²) / u)² =
+
+#distribute the exponent to all the factors ➜
+    (-7)²(w²)² / (u)² =
+
+    [(49(w⁴)) / u²]
+    
+'''
+next
+'''
+    (3 / 7)⁻¹ =
+
+#apply the negative exponent rule ➜ flip the numerator and denominator and change the exponent to positive ➜
+    (7 / 3)¹ =
+    [7 / 3]
+
+'''
+next problem
+'''
+    f(x) = | 6x | + (-8)
+    find f(-(1/2)) 
+
+    = | 6(-(1/2)) | + (-8)
+    = | -3 | + (-8)
+    = 3 + (-8)
+    [= (-5)]
+
+    g(x) = x / (x² + (-10))
+    find g(4)
+
+    = 4 / (4² + (-10))
+    = 4 / (16 + (-10))
+    = 4 / 6
+#divide by gcf ➜
+    [= 2 / 3]
+
+    h(x) = √‾‾((-7x) + 1)
+    find h(-2)
+
+    = √‾‾((-7(-2)) + 1)
+    = √‾‾((14) + 1)
+    [= √‾‾(15)]
+    
+'''
+next
+'''
+    f(x) = 3x² + (-3)
+    find f(4y)
+
+    = 3(4y)² + (-3)
+    = 3(16y²) + (-3)
+#just regular multiplication, not distribution ➜
+    = 48y² + (-3)
+    
+'''
+next
+'''
+#distance formula ➜ √‾‾(x₂ - x₁)² + (y₂ - y₁)²
+
+((-2), (-1))
+((-7), 5)
+
+√‾‾(-7 - -2)² + (5 - -1)² =
+
+#double negatives become positive ➜
+√‾‾(-5)² + 6² =
+√‾‾25 + 36 =
+√‾‾61
+    
+'''
+next
+'''
+    x⁰ = 1 where x ≠ 0
+    
+'''
+next
+'''
+    (-6)  ≥  4 + (-5v)
+    (-4)   (-4)
+ #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+    (-10)  ≥  (-5v)
+   / (-5)   / (-5)
+ #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+ #dividing an inequality by a negative flips the sign
+    [2  ≤  v]
+
---
+++ weeks-3-and-4/quadratics.py
@@ -0,0 +1,204 @@
+    
+'''
+next
+'''
+    7y² + (-5y) + (-2)
+
+#quadratic expression ➜ ax² + bx + c
+    a = 7
+    b = -5
+    c = -2
+    
+#need two numbers that multiply to (a * c) and add up to b
+    a * c = (-14)
+    b = (-5)
+    (-7), 2
+
+#rewrite the middle term using those  numbers 
+    7y² + (-7y) + (2y) + (-2)
+
+#split the expression into two pairs
+    (7y² + (-7y))  +  ((2y) + (-2))
+
+#extract the common factors 
+    7y(y + (-1)) + 2(y + (-1))
+
+#factor out the common binomial
+    [(7y + 2)(y + (-1))]
+    
+'''
+next
+'''
+    5y² + (-17y) + 6
+
+#quadratic expression ➜ ax² + bx + c
+    a = 5
+    b = (-17)
+    c = 6
+    
+#need two numbers that multiply to (a * c) and add up to b
+    a * c = (30)
+    b = (-17)
+    (-15), (-2)
+
+#rewrite the middle term using those  numbers 
+    5y² + (-15y) + (-2y) + 6
+
+#split the expression into two pairs
+    (5y² + (-15y))  +  ((-2y) + 6)
+
+#extract the common factors 
+    5y(y + (-3)) + (-2)(y + (-3))
+
+#factor out the common binomial
+    [(5y + (-2))(y + (-3))]
+    
+'''
+next
+'''
+    3z² + 14z + (-5)
+
+#quadratic expression ➜ ax² + bx + c
+    a = 3
+    b = 14
+    c = (-5)
+    
+#need two numbers that multiply to (a * c) and add up to b
+    a * c = (-15)
+    b = 14
+    (15), (-1)
+
+#rewrite the middle term using those  numbers 
+    3z² + (15z) + (-1z) + (-5)
+
+#split the expression into two pairs
+    (3z² + (15z))  +  ((-1z) + (-5))
+
+#extract the common factors 
+    3z(z + 5) + (-1)(z + 5)
+
+#factor out the common binomial
+    [(3z + (-1))(z + 5)]
+    
+'''
+next
+'''
+    6a² + 4a
+#gcf of both coefficients and variables ➜
+    2a
+#divide each term by the gcf ➜
+    [2a(3a + 2)]
+    
+'''
+next
+'''
+    12y² + (-13y³)
+#gcf of both coefficients and variables ➜
+    y²
+#divide each term by the gcf ➜
+    [y²(12 + (-13y))]  
+    
+'''
+next
+'''
+    9x² + 12x³
+#gcf of both coefficients and variables ➜
+    3x²
+#divide each term by the gcf ➜
+    [3x²(3 + 4x)]
+    
+'''
+next
+'''
+    2y² + 13y + 20
+
+#quadratic expression ➜ ax² + bx + c
+    a = 2
+    b = 13
+    c = 20
+    
+#need two numbers that multiply to (a * c) and add up to b
+    a * c = (40)
+    b = 13
+    8, 5
+
+#rewrite the middle term using those  numbers 
+    2y² + 8y + 5y + 20
+
+#split the expression into two pairs
+    (2y² + 8y)  +  (5y + 20)
+
+#extract the common factors 
+    2y(y + 4) + 5(y + 4)
+
+#factor out the common binomial
+    [(2y + 5)(y + 4)]
+    
+'''
+next
+'''
+    4z² + (-12z) + (-7)
+
+#quadratic expression ➜ ax² + bx + c
+    a = 4
+    b = (-12)
+    c = (-7)    
+    
+#need two numbers that multiply to (a * c) and add up to b
+    a * c = (-28)
+    b = (-12)
+    (-14), 2
+
+#rewrite the middle term using those  numbers 
+    4z² + (-14z) + 2z + (-7)
+
+#split the expression into two pairs
+    (4z² + (-14z))  +  (2z + (-7))
+
+#extract the common factors 
+    2z(2z + (-7)) + 1(2z + (-7))
+
+#factor out the common binomial
+    [(2z + 1)(2z + (-7))]
+    
+'''
+next
+'''
+    3x² + 1x + (-24)
+
+#quadratic expression ➜ ax² + bx + c
+    a = 3
+    b = 1
+    c = (-24)    
+    
+#need two numbers that multiply to (a * c) and add up to b
+    a * c = (-72)
+    b = 1
+    9, (-8)
+
+#rewrite the middle term using those  numbers 
+    3x² + 9x + (-8x) + (-24)
+
+#split the expression into two pairs
+    (3x² + 9x)  +  ((-8x) + (-24))
+
+#extract the common factors 
+    3x(x + 3) + (-8)(x + 3)
+
+#factor out the common binomial
+    [(3x + (-8))(x + 3)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
