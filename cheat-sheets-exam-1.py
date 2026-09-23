2f26f991c02606dc00624ab1b0fa59e050777e63
Kelsey Crow <kelseybcrow@gmail.com>
2026-09-23 13:00:42 -0600

    wednesday afternoon

--- cheat-sheets-exam-1.py
+++ cheat-sheets-exam-1.py
@@ -16,5 +16,8 @@ horizontal  ➜  y = f(x - h)
 '''
 #translation
     y  =  (-x)²  ➜   y  =  -(x - 3)² + 1
+    'translation-9-23.png'
 
-os.startfile('translation-9-23.png')
+#explain what it means for a relation to be a function ➜  
+    #each input ÷ domain ÷ x-value must map to only one single output ÷ range ÷ y-value
+    #the vertical line test is an easy way to check
--- formative-assessments/formative-week-5.py
+++ formative-assessments/formative-week-5.py
@@ -1,20 +1,18 @@
 Kelsey Crow
+  
 '''
-I only knew how to do the four relations problem on the back of the sheet without consulting my notes from similar problems in ALEKS. 
-The problems on the front, I completed by referencing my notes. 
+problem 1
 '''
-
-import os    
 '''
-next
+for this problem, I had to consult my notes from similar problems in ALEKS
 '''
 #translation
     y  =  (-x)²  ➜   y  =  -(x - 3)² + 1
 
-os.startfile('translation-9-23.png')
 #vertical translation  ➜  addition on the outside, so 1 unit upward
 #horizontal translation  ➜  subtraction on the inside, so 3 units to the right
 #vertical reflection because  ➜  negative sign goes from inside to outside
+('translation-9-23.png')
     
 '''
 TRANSLATIONS CHEAT SHEET  
@@ -30,5 +28,29 @@ horizontal  ➜  y = f(x - h)
             ➜  moving left: add units inside
             ➜  moving right: subtract units inside
             ➜  horizontal reflection when negative sign goes from in-side to out-side
-
 '''
+    
+'''
+problem 2 ➜
+'''
+    y = | x |  ➜  y = | x - 2 | - 3
+
+#vertical translation  ➜  subtraction on the outside, so move points downward by 3 units
+#horizontal translation  ➜  subtraction on the inside, so move points right by 3 units
+#no reflection because  ➜  no beginning negative sign that got moved
+('2nd-translation-9-23.png')
+    
+'''
+problem 3 ➜
+'''
+    
+#explain what it means for a relation to be a function ➜  
+    #each input / domain / x-value must map to only one single output / range / y-value
+    #the vertical line test is an easy way to check
+
+#define a function ➜  same as above  
+
+
+
+
+
